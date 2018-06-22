7ea53b6fbaa6dfb36bbc21cf01da47bc472ae88d
Malith Prasanna Rathnasena <malithofficial@gmail.com>
2018-06-22 23:47:44 +0530

    update

--- Phara.py
+++ Phara.py
@@ -6,10 +6,12 @@ import glob
 myDataList = []
 
 class Data:
-    def __init__(self, pageNo, lineNo, image):
+    def __init__(self, pageNo, lineNo, status, image):
         self.pageNo = pageNo
         self.lineNo = lineNo
+        self.status = status
         self.image = image
+
 
     def __str__(self):
         return self
@@ -19,10 +21,10 @@ def lineseperate(image_list):
     for page, img in enumerate(image_list):
         page = page+1
         line = 0
-        print(page, img.replace("\\", "/"))
+        #print(page, img.replace("\\", "/"))
         large = cv2.imread(img.replace("\\", "/"))
         rgb = large
-        print('rgb shape ',rgb.shape)
+        #print('rgb shape ',rgb.shape)
         small = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
 
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
@@ -30,7 +32,7 @@ def lineseperate(image_list):
 
         _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 10))
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 6))
         connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
 
         # using RETR_EXTERNAL instead of RETR_CCOMP
@@ -48,21 +50,23 @@ def lineseperate(image_list):
             if r > 0.45 and w > 20 and h > 8:
                 rgb = cv2.rectangle(rgb, (x, y), (x + w - 1, y + h - 1), (0, 0, 255), 1)
                 roi = small[y:y + h, x:x + w]
-
+                cv2.imshow('rec', rgb)
+                cv2.waitKey(0)
 
                 if h > 8 and h < 30:
 
-                    #cv2.imshow('roi', roi)
-                    #cv2.waitKey(0)
-                    print('img shape ', roi.shape)
-                    data = Data(page, line, roi)
+
+                    status = ''
+                    #print('img shape ', roi.shape)
+                    data = Data(page, line, status, roi)
                     myDataList.append(data)
 
 
                 if h > 30:
-                    print('line no into paraseperate ',line)
+                    #print('line no into paraseperate ',line)
                     line_no = paraseperate(roi,page,line)
                     line = line_no
+
 
             line = line + 1
 
@@ -70,7 +74,7 @@ def lineseperate(image_list):
         #cv2.waitKey(0)
 
     for elements in myDataList:
-        name = "page_%d_line_%d.jpg" % (elements.pageNo, elements.lineNo)
+        name = "page_%d_line_%d_%s.jpg" % (elements.pageNo, elements.lineNo, elements.status)
         path = 'C:/Users/Malith/PycharmProjects/untitled/output'
         cv2.imwrite(os.path.join(path, name), elements.image)
     print("Seperation completed")
@@ -97,6 +101,7 @@ def paraseperate(img,page,line):
     contours = list(reversed(contours))
     #cv2.imshow('roi w', img)
     #cv2.waitKey(0)
+    length = len(contours)
     for idx in range(len(contours)):
 
         x, y, w, h = cv2.boundingRect(contours[idx])
@@ -104,22 +109,28 @@ def paraseperate(img,page,line):
         cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
         r = float(cv2.countNonZero(mask[y:y + h, x:x + w])) / (w * h)
         if r > 0.45 and w > 20 and h > 8:
-
+            status = ''
             rgb1 = cv2.rectangle(rgb1, (x, y), (x + w - 1, y + h - 1), (255, 255, 255), 0)
             roi1 = small1[y:y + h, x:x + w]
-            data = Data(page, line, roi1)
+            if idx == 0:
+                status = 'P'
+
+            if idx == length :
+                status = '<closeP'
+
+            data = Data(page, line, status, roi1)
             myDataList.append(data)
             line = line + 1
 
     #cv2.imshow('contours3', img)
     #cv2.waitKey(0)
-    print('line no pass',line)
+    #print('line no pass',line)
     return line
 
 
 
 def main(folder):
     image_list = (glob.glob(folder + "/*.jpg"))
-    print(image_list)
+    #print(image_list)
     lineseperate(image_list)
 
--- test2.py
+++ test2.py
@@ -1,45 +1,53 @@
 import os
 import sys
-from PyQt5.QtWidgets import QApplication, QTextEdit, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QFileDialog, QLabel
 import Phara
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 
 class Notepad(QWidget):
     filename = ''
     def __init__(self):
         super(Notepad, self).__init__()
-
+        self.setGeometry(100, 100, 800, 480)
         self.init_ui()
 
     def init_ui(self):
+        self.setWindowTitle('Book Digitizer Platform')
+        self.pylabel = QLabel('Book Digitizer Platform', self)
+        self.pylabel.resize(400, 50)
+        self.pylabel.move(200, 25)
+        self.pylabel.setFont(QFont('SansSerif', 20, weight=QFont.Bold))
 
-
-        pylabel1 = QLabel('Select the book', self)
-        pylabel1.resize(200, 32)
-        pylabel1.move(25, 50)
+        self.pylabel1 = QLabel('Select the book', self)
+        self.pylabel1.resize(200, 32)
+        self.pylabel1.move(25, 150)
+        self.pylabel1.setFont(QFont('SansSerif', 10))
 
         pybutton1 = QPushButton('Choose', self)
         pybutton1.resize(200, 32)
-        pybutton1.move(150, 50)
+        pybutton1.move(200, 150)
         pybutton1.clicked.connect(self.open_folder)
 
-        self.pyedit = QLabel(self)
-        self.pyedit.resize(400, 32)
-        self.pyedit.move(375, 50)
-        #self.pyedit.setReadOnly(True)
+        self.pylabel2 = QLabel(self)
+        self.pylabel2.resize(400, 32)
+        self.pylabel2.move(450, 150)
 
-        pybutton2 = QPushButton('Submit', self)
-        pybutton2.resize(200, 50)
-        pybutton2.move(350, 150)
-        pybutton2.clicked.connect(self.submit)
+        self.pybutton2 = QPushButton('Submit', self)
+        self.pybutton2.resize(200, 50)
+        self.pybutton2.move(300, 250)
+        self.pybutton2.setFont(QFont('SansSerif', 12))
+
+        self.pybutton2.clicked.connect(self.submit)
 
         self.show()
 
 
 
     def open_folder(self):
-        self.filename = QFileDialog.getExistingDirectory(self, 'Select Directory')
-        self.pyedit.setText(self.filename)
+        self.filename = QFileDialog.getExistingDirectory(self,'Select Directory')
+        self.pylabel2.setText(self.filename)
 
     def submit(self):
         Phara.main(self.filename)
